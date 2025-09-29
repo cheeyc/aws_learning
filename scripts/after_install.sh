0fa6c0934696b5e6b7b8937f85a694b7b8ed5b4a
yongcheng.chee <yongcheng.chee@zurich.com.my>
2025-09-29 23:04:27 +0800

    Initial commit

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,4 +6,8 @@ then
   curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
   sudo yum install -y nodejs
 fi
-npm install --production
+sudo chown -R ec2-user:ec2-user /home/ec2-user/my-node-app
+sudo chmod -R 755 /home/ec2-user/my-node-app
+
+# Install dependencies
+sudo -u ec2-user npm install --omit=dev
