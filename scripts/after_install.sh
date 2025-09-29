6532d85129aaea2c3c3570db384e62d73f635a4e
yongcheng.chee <yongcheng.chee@zurich.com.my>
2025-09-29 23:00:34 +0800

    Initial commit

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd /home/ec2-user/my-node-app || exit
+if ! command -v node &> /dev/null
+then
+  echo "Installing Node.js..."
+  curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
+  sudo yum install -y nodejs
+fi
 npm install --production
